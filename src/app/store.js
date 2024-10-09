5411b6e4da6200d9f70de690cc83d8ebb898ba32
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-10-09 20:35:02 +0530

    Redux-Persist used to saving data to localStorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import expenseReducer from "../features/expenses/expensesSlice"
+import storage from "redux-persist/lib/storage" //defaults to localStorage
+import { persistStore, persistReducer } from "redux-persist"
+
+
+const persistConfig = {
+    key: 'expenses',
+    storage, //uses localStorage for persisting state
+}
+
+const persistedReducer = persistReducer(persistConfig, expenseReducer)
 
 export const store = configureStore({
-    reducer: expenseReducer
+    reducer: persistedReducer
 })
+
+export const persistor = persistStore(store)
