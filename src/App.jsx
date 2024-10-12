d37fab225a2294a9e51796aa0951ecebbb7841c7
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-10-12 12:46:12 +0530

    styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PieChart from "./components/PieChart";
 
 function App() {
   return (
-    <div className="rounded-lg w-[21rem] md:w-[30rem] px-6 pt-1 pb-8 md:pb-5 bg-gray-900 my-10 md:m-4">
+    <div className="rounded-lg w-[22rem] md:w-[30rem] px-6 pt-1 pb-8 md:pb-5 bg-gray-900 my-10 md:m-4">
       <Header />
       <Balance />
       <IncomeExpense />
